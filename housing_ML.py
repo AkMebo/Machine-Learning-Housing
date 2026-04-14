be41a039d86065f8173ac1ab7023a2b2aeb8f5d0
AkMebo <117305774+AkMebo@users.noreply.github.com>
2026-04-14 05:13:35 +0000

    update

--- housing_ML.py
+++ housing_ML.py
@@ -1,1 +1,15 @@
 
+#Example
+import pandas as pd 
+import kagglehub
+from kagglehub import KaggleDatasetAdapter
+
+file_name = 'melb_data.csv'
+
+melbourne_housing_df = kagglehub.dataset_load(
+    KaggleDatasetAdapter.PANDAS,
+    "dansbecker/melbourne-housing-snapshot",
+    file_name,
+)
+
+melbourne_housing_df.describe()
