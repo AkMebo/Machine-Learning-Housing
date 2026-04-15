49f839cf4f8efd4893975edfc43f46edec0e32fb
AkMebo <117305774+AkMebo@users.noreply.github.com>
2026-04-15 05:04:47 +0000

    update - creating a model using scikit-learn

--- housing_ML.py
+++ housing_ML.py
@@ -1,6 +1,4 @@
-
 #Example
-import pandas as pd 
 import kagglehub
 from kagglehub import KaggleDatasetAdapter
 
@@ -13,3 +11,30 @@ melbourne_housing_df = kagglehub.dataset_load(
 )
 
 melbourne_housing_df.describe()
+print(melbourne_housing_df.columns)
+
+#drop na
+melbourne_housing_data = melbourne_housing_df.dropna(axis=0)
+
+#select the prediction target (dot notation) - pull out a variable
+y = melbourne_housing_data.Price
+
+#the features - factors that affect the y (x)
+melbourne_features = ['Rooms', 'Bathroom', 'Landsize', 'Lattitude', 'Longtitude']
+x = melbourne_housing_data[melbourne_features]
+x.describe()
+x.head()
+
+#scikit-learn used to create models
+from sklearn.tree import DecisionTreeRegressor #pip install scikit-learn
+
+# Define model. Specify a number for random_state to ensure same results each run
+# random_state = 1, ensures you get same results in each run. However, ML models allow randomness in model training
+melbourne_model = DecisionTreeRegressor(random_state=1)  
+# Fit model
+melbourne_model.fit(x, y)
+
+print("Making predictions for the following 5 houses:")
+print(x.head())
+print("The predictions are")
+print(melbourne_model.predict(x.head()))
