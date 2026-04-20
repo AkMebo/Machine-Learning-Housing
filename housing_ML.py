bafb4e61623d0f720616f652c0324b325715ae10
AkMebo <117305774+AkMebo@users.noreply.github.com>
2026-04-20 05:06:30 +0000

    update on the model fitting

--- housing_ML.py
+++ housing_ML.py
@@ -38,3 +38,25 @@ print("Making predictions for the following 5 houses:")
 print(x.head())
 print("The predictions are")
 print(melbourne_model.predict(x.head()))
+
+#How good is the MODEL
+#Mean Absolute Error (MAE)
+
+from sklearn.metrics import mean_absolute_error
+#prediction error
+predicted_home_prices = melbourne_model.predict(x)
+print(mean_absolute_error(y, predicted_home_prices))
+
+# trainig data vs validation data 
+from sklearn.model_selection import train_test_split
+
+# split data into training and validation data, for both features and target The split is based on a random number generator. 
+# Supplying a numeric value to the random_state argument guarantees we get the same split every time we run this script.
+train_x, val_x, train_y, val_y = train_test_split(x, y, random_state = 0)
+# Define model
+melbourne_model = DecisionTreeRegressor()
+# Fit model
+melbourne_model.fit(train_x, train_y)
+# get predicted prices on validation data
+val_predictions = melbourne_model.predict(val_x)
+print(mean_absolute_error(val_y, val_predictions))
